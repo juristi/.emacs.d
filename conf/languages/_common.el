79fc0da23a67230f693358d14d79212c5d426d8b
Juha Ristimäki <juha.ristimaki@gmail.com>
2018-02-06 17:59:12 +0200

    Add and enable flycheck

---
+++ conf/languages/_common.el
@@ -0,0 +1,10 @@
+;; _common.el
+;;
+;; Configure and enable general development packages.
+;; This file is not part of GNU Emacs
+
+(global-flycheck-mode)
+
+(provide '_common)
+
+;; End of file
--- conf/languages/languages.el
+++ conf/languages/languages.el
@@ -3,6 +3,8 @@
 ;; Programming language specific configurations and hooks.
 ;; This file is not part of GNU Emacs
 
+(require '_common)
+
 (require '_javascript)
 (require '_lisp)
 
