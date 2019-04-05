7ae3f4ca2d6678e71e51e60329faf6f9422cd887
Juha Ristimäki <juha.ristimaki@gmail.com>
2019-04-05 15:15:58 +0300

    Use js2-jsx mode for .jsx files

--- conf/languages/_javascript.el
+++ conf/languages/_javascript.el
@@ -9,6 +9,9 @@
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 (add-to-list 'interpreter-mode-alist '("node" . js2-mode))
 
+(add-to-list 'auto-mode-alist '("\\.jsx\\'" . js2-jsx-mode))
+(add-to-list 'interpreter-mode-alist '("node" . js2-jsx-mode))
+
 (add-hook 'js2-mode-hook #'js2-refactor-mode)
 
 (js2r-add-keybindings-with-prefix "C-c C-r")
